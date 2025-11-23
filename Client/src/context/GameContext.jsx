2f8cdee353388c1181c0db84320b257f5075b918
vaib65 <vhadke.02@gmail.com>
2025-11-23 10:16:55 +0530

    Update GameContext.jsx

--- Client/src/context/GameContext.jsx
+++ Client/src/context/GameContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://guess-it-server.onrender.com");
 
 export const GameContext = createContext({
   username: "",
