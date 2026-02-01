0723b4e316c57fc0ed5ed63515414f79d1965c50
vaibhav H <vhadke.02@gmail.com>
2026-02-01 21:23:38 +0530

    Fix SinglePlayer import casing for Frame component

--- Client/src/pages/single/SinglePlayer.jsx
+++ Client/src/pages/single/SinglePlayer.jsx
@@ -1,4 +1,4 @@
-import Frame from "../../components/Single/Frame.jsx";
+import Frame from "../../components/single/Frame.jsx";
 import { useEffect, useState } from "react";
 import {
   startSinglePlayer,
